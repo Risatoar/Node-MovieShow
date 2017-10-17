60e481cf267c88d2d7c9a8f9308309c2ffb64984
Xu Xiang <risatoar@163.com>
2017-10-17 17:15:45 +0800

    页面制作完成

--- app.js
+++ app.js
@@ -1,33 +1,166 @@
 var express = require('express')
+var path = require('path')
+var mongoose = require('mongoose')
+var _ = require('underscore')
+var bodyParser = require('body-parser')
+var Movie = require('./models/movie')
 var port = process.env.PORT || 3000
 var app = express()
 
-app.set('views','./views')
-app.set('view engine','jade')
+mongoose.connect('mongodb://localhost:27017/wuch',{useMongoClient: true})
+
+app.set('views','./views/pages')
+app.set('view engine', 'jade')
+app.use(bodyParser.urlencoded())
+app.use(express.static(path.join(__dirname, 'public')))
+app.locals.moment = require('moment')
 app.listen(port)
 
-console.log('started on port' + port)
+console.log('node start on port' + port)
 
-app.get('/',function (req,res) {
-	res.render('index',{
-		title:'首页'
-	})
+// index page
+app.get('/', function(req, res) {
+    Movie.fetch(function(err, movies) {
+        if (err) {
+            console.log(err)
+        }
+        res.render('index', {
+            title: '爱电影 首页',
+            movies: movies
+        })
+    }) 
 })
 
-app.get('/movie/:id',function (req,res) {
-	res.render('detail',{
-		title:'详情页'
-	})
+// admin update movie
+app.get('/admin/update/:id', function(req, res) {
+    var id = req.params.id
+
+    if(id) {
+        Movie.findById(id, function(err, movie) {
+            if(err) {
+                console.log(err)
+            }
+            res.render('admin', {
+                title: '爱电影 后台更新页',
+                movie: movie
+            })
+        })
+    }
 })
 
-app.get('/admin/movie',function (req,res) {
-	res.render('admin',{
-		title:'后台录入页'
-	})
+//admin movie
+app.get('/admin/movie', function(req, res) {
+    res.render('admin', {
+        title:'后台录入页面',
+        movie: {
+            title: '',
+            doctor: '',
+            country: '',
+            year: '',
+            poster: '',
+            summary: '',
+            flash: '',
+            language: ''
+        }
+    })
 })
 
-app.get('/admin/list',function (req,res) {
-	res.render('list',{
-		title:'列表页'
-	})
+//admin post movie
+app.post('/admin/movie/new', function(req, res) {
+    var id = req.body.movie._id
+    var movieObj = req.body.movie
+    var _movie
+
+    if (id !== 'undefined') {
+        Movie.findById(id, function(err, movie) {
+            if(err) {
+                console.log(err)
+            }
+
+            _movie = _.extend(movie, movieObj)
+            _movie.save(function(err, movie) {
+                if(err) {
+                    console.log(err)
+                }
+                res.redirect('/movie/' + movie._id)
+            })
+        })
+    }
+    else {
+        _movie = new Movie({
+            doctor: movieObj.doctor,
+            title: movieObj.title,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        })
+
+        _movie.save(function(err, movie) {
+            if(err) {
+                console.log(err)
+            }
+            res.redirect('/movie/' + movie._id)
+        })
+    }
 })
+
+// list page
+app.get('/admin/list', function(req, res) {
+    Movie.fetch(function(err, movies) {
+        if(err) {
+            console.log(err)
+        }
+        res.render('list', {
+            title: '爱电影 列表页',
+            movies: movies
+        })
+    })
+})
+
+// detail page
+app.get('/movie/:id', function(req, res) {
+    var id = req.params.id
+
+    Movie.findById(id, function(err, movie){
+        res.render('detail', {
+            title: '爱电影 ' + movie.title,
+            movie: movie
+        })
+    })
+})
+
+//list delete movie
+app.delete('/admin/list', function(req, res) {
+    var id = req.query.id
+
+    if(id) {
+        Movie.remove({_id: id}, function(err, movie) {
+            if(err) {
+                console.log(err)
+            }
+            else {
+                res.json({success: 1})
+            }
+        })
+    }
+})
+
+// update page
+// app.get('/admin/update/:id', function(req, res) {
+//     res.render('admin', {
+//         title: '爱电影 后台录入页',
+//         movie: {
+//             title: '',
+//             doctor: '',
+//             country: '',
+//             year: '',
+//             poster: '',
+//             flash: '',
+//             summary: '',
+//             language: ''
+//         }
+//     })
+// })
